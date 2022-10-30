a4d58e63856badbe6ea47ef9481fb5fd333d9d8f
drizzy009 <yusid03@gmail.com>
2022-10-30 15:32:08 +0100

    social section

--- src/components/SocialSection.tsx
+++ src/components/SocialSection.tsx
@@ -2,7 +2,7 @@ import { Icon, slack } from '../assets';
 
 const SocialSection = () => {
   return (
-    <div className="flex flex-col items-center gap-16 py-6">
+    <div className="flex flex-col items-center gap-16">
       <div className="flex flex-col items-center gap-8 px-8">
         <div className="flex flex-row items-center p-0">
           <div className="flex flex-row justify-center gap-6 p-0">
