b83d4ca37f17aa58a3ea890e3c09a8068a633ea9
drizzy009 <yusid03@gmail.com>
2022-10-30 13:38:04 +0100

    profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,7 @@ const Profile = () => {
       <div  className='flex flex-col items-center p-0 isolate'>
         <img src={profile__img} alt="Zuri" className="w-[88px] h-[88px] flex flex-col justify-center items-center p-0" />
         <h3 className="pt-6 text-lg font-bold text-center text-gray-900 font-inter">Annette Black</h3>
-        <img className='absolute w-[20px] h-[20px] right-[208px] top-[20px]' src={arrow} alt="" />
+        <img className='absolute w-[20px] h-[20px] right-[208px] top-[20px] cursor-pointer' src={arrow} alt="" />
       </div>
     </div>
   );
