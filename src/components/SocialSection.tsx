1c62efd46cb7cc7414b29c82821f6354d3019f1a
drizzy009 <yusid03@gmail.com>
2022-10-31 13:23:25 +0100

    new

--- src/components/SocialSection.tsx
+++ src/components/SocialSection.tsx
@@ -6,7 +6,7 @@ const SocialSection = () => {
     <div className={`flex flex-col items-center gap-16 ${styles.socialsecBottom}`}>
       <div className="flex flex-col items-center gap-8 px-8">
         <div className="flex flex-row items-center p-0">
-          <div className="flex flex-row justify-center gap-6 p-0">
+          <div className="flex flex-row justify-center gap-6 p-6">
             <img src={slack} alt="slack_icon" />
             <img src={Icon} alt="github_icon" />
           </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,22 +2,27 @@ import { I4G } from '../assets';
 
 export const profileLinks = [
   {
-    id: 'btn__zuri',
+    id: 'twitter',
     title: 'Twitter Link',
+    links: 'https://twitter.com/drizzy0x_',
+  },
+  {
+    id: 'btn__zuri',
+    title: 'Zuri Team',
     links: 'https://training.zuri.team/',
   },
   {
     id: 'books',
-    title: 'Zuri Team',
+    title: 'Zuri Books',
     links: 'http://books.zuri.team/',
   },
   {
     id: 'book__python',
-    title: 'Zuri Books',
+    title: 'Python Books',
     links: 'https://books.zuri.team/python-for-beginners?ref_id=drizzy0x',
   },
   {
-    id: 'pythonbooks',
+    id: 'pitch',
     title: 'Background Check for Coders',
     links: 'https://background.zuri.team/',
   },
